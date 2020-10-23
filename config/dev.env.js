aa43b23ef76aed293ac27298c82e65003eb24def
Ali Mamaev <mamaev-ali@inbox.ru>
2020-10-24 02:09:09 +0300

    https

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://skfu.spichka.dev"',
-  FILE_URL: '"http://skfu.spichka.dev"',
+  BASE_API: '"https://skfu.spichka.dev"',
+  FILE_URL: '"https://skfu.spichka.dev"',
   SOCKET_PATH: '"/api/v1/socket.io"',
 })
